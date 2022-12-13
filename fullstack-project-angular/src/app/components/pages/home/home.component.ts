e2c7e81e22c51ef4ec421e69aa3ce14699e2635f
Juan Blanco <juan.x.blanco@timken.com>
2022-12-12 19:16:39 -0600

    feature: display favorites on home page

--- fullstack-project-angular/src/app/components/pages/home/home.component.ts
+++ fullstack-project-angular/src/app/components/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from '@shared/services/local-storage.service';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  charactersFav$ = this.localStorageSvc.charactersFav$;
 
-  constructor() { }
+  constructor(private localStorageSvc: LocalStorageService) { }
 
   ngOnInit(): void {
   }
